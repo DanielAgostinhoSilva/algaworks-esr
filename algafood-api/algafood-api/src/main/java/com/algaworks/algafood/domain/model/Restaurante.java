cf3f03b8b6c8fae97100d16c620e97069f906f94
Daniel <daniel.agostinho@hotmail.com>
2021-02-13 17:35:55 -0300

    3.4. Mapeando entidades com JPA

---
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode( onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+public class Restaurante {
+    @EqualsAndHashCode.Include
+    @Id
+    private Long id;
+
+    private String nome;
+
+    @Column(name="taxa_frete")
+    private BigDecimal taxaFrete;
+}
