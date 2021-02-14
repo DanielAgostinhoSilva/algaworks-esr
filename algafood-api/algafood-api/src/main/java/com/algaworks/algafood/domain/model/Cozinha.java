2718fc6771a1596a9b04da21af1f8a7e2569bb57
Daniel <daniel.agostinho@hotmail.com>
2021-02-13 22:24:25 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

--- algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,20 +1,15 @@
 package com.algaworks.algafood.domain.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-@Getter
-@Setter
-@ToString
+@Data
 @EqualsAndHashCode( onlyExplicitlyIncluded = true)
+@Entity
 public class Cozinha {
 
 
