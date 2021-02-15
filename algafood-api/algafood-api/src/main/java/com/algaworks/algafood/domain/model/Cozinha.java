37fba03e9d290ffa5b245636e7266e8b8866f96b
Daniel <daniel.agostinho@hotmail.com>
2021-02-15 15:52:04 -0300

    3.19. Propriedade nullable de @Column e @JoinColumn

--- algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,10 +2,7 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode( onlyExplicitlyIncluded = true)
@@ -17,5 +14,7 @@ public class Cozinha {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column( name = "nome", nullable = false)
     private String nome;
 }
