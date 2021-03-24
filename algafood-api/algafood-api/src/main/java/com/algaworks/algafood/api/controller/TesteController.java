c2f3654e7490e44121772baa85afc56ba4cbe10e
Daniel <daniel.agostinho@hotmail.com>
2021-03-24 20:43:59 -0300

    5.7. Usando as keywords para definir critérios de query methods

--- algafood-api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -19,8 +22,16 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
     @GetMapping("/cozinha/por-nome")
     public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-        return cozinhaRepository.findByNome(nome);
+        return cozinhaRepository.findByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurante/por-taxa-frete")
+    public List<Restaurante> cozinhasPorNome(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
     }
 }
