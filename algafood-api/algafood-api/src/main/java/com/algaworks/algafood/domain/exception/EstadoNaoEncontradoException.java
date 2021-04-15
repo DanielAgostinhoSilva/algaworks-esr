d12d56971019b084991d6d4ad7ee7d3f49741f14
Daniel <daniel.agostinho@hotmail.com>
2021-04-14 21:30:07 -0300

    8.10. Afinando a granularidade e definindo a hierarquia das exceptions de negócios

---
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EstadoNaoEncontradoException.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.domain.exception;
+
+public class EstadoNaoEncontradoException extends EntidadeNaoEncontradaException {
+    private static final String MSG_ESTADO_NAO_ENCONTRADO = "Nao existe um cadastro de estado com codigo %d";
+
+    public EstadoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
+    public EstadoNaoEncontradoException(Long estadoId) {
+        this(String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
+    }
+}
--- algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 import lombok.AllArgsConstructor;
@@ -14,26 +14,24 @@ import org.springframework.stereotype.Service;
 public class CadastroEstadoService {
 
     private final String MSG_ESTADO_EM_USO = "Estado de codigo %d nao pode ser removido, pois esta em uso";
-    private final String MSG_ESTADO_NAO_ENCONTRADO = "Nao existe um cadastro de estado com codigo %d";
     private EstadoRepository estadoRepository;
 
     public Estado salvar(Estado estado) {
         return estadoRepository.save(estado);
     }
 
-    public void excluir(Long id) {
+    public void excluir(Long estadoId) {
         try {
-            estadoRepository.deleteById(id);
+            estadoRepository.deleteById(estadoId);
         } catch (EmptyResultDataAccessException e) {
-            throw new EntidadeNaoEncontradaException(String.format(MSG_ESTADO_NAO_ENCONTRADO, id));
+            throw new EstadoNaoEncontradoException(estadoId);
         } catch (DataIntegrityViolationException e) {
-            throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, id));
+            throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, estadoId));
         }
     }
 
     public Estado buscarOuFalhar(Long estadoId) {
         return estadoRepository.findById(estadoId)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException(
-                        String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
+                .orElseThrow(() -> new EstadoNaoEncontradoException(estadoId));
     }
 }
