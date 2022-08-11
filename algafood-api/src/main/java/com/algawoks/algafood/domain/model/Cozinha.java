bb8a0d061387d40307d5cc1afbcf16e067bc2722
Daniel <daniel.agostinho@hotmail.com>
2022-08-10 22:25:09 -0300

    feat: Mapeando o id da entidade para autoincremento

--- algafood-api/src/main/java/com/algawoks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algawoks/algafood/domain/model/Cozinha.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 @Entity
 public class Cozinha {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
     private String nome;
--- algafood-api/src/main/java/com/algawoks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algawoks/algafood/domain/model/Restaurante.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
@@ -12,6 +14,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Restaurante {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
     private String nome;
