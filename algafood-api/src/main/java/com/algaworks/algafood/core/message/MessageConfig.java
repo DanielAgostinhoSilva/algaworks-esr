2c00e6611cd472a8e81ed3c12833fd95db27395c
Daniel <daniel.agostinho@hotmail.com>
2022-08-21 16:30:37 -0300

    feat: Refatorando serviço de cadastro de restaurante para incluir endereço

---
+++ algafood-api/src/main/java/com/algaworks/algafood/core/message/MessageConfig.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.core.message;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(1);
+        return messageSource;
+    }
+}
--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
@@ -13,13 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class CadastroRestauranteService {
     private RestauranteRepository restauranteRepository;
     private CadastroCozinhaService cadastroCozinha;
+    private CadastroCidadeService cadastroCidade;
 
     public Restaurante salvar(Restaurante restaurante) {
         Long cozinhaId = restaurante.getCozinha().getId();
+        Long cidadeId = restaurante.getEndereco().getCidade().getId();
 
         Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
+        Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
 
         restaurante.setCozinha(cozinha);
+        restaurante.getEndereco().setCidade(cidade);
 
         return restauranteRepository.save(restaurante);
     }
