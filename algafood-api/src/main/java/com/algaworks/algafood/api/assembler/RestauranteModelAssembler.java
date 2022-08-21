4ea68287ce7f4603908444df1aa8591679e87461
Daniel <daniel.agostinho@hotmail.com>
2022-08-21 14:37:45 -0300

    feat: Adicionando endereço no modelo da representação do recurso de restaurante

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.Restaurante;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -9,20 +11,32 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor
 @Component
 public class RestauranteModelAssembler {
 
-	private ModelMapper modelMapper;
-	
+	private final ModelMapper modelMapper;
+
+	public RestauranteModelAssembler(ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+		configureMapping();
+	}
+
 	public RestauranteModel toModel(Restaurante restaurante) {
 		return modelMapper.map(restaurante, RestauranteModel.class);
 	}
 	
 	public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
 		return restaurantes.stream()
-				.map(restaurante -> toModel(restaurante))
+				.map(this::toModel)
 				.collect(Collectors.toList());
+	}
+
+	private void configureMapping(){
+		modelMapper.createTypeMap(Endereco.class, EnderecoModel.class)
+				.<String>addMapping(
+						endereco -> endereco.getCidade().getEstado().getNome(),
+						((enderecoModel, value) -> enderecoModel.getCidade().setEstado(value))
+				);
 	}
 	
 }
--- algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
