35289ca6380896aeb239fa6ad3a9341c05a125fe
Daniel <daniel.agostinho@hotmail.com>
2022-09-11 12:11:45 -0300

    feat: 12.20 Otimizando a query de pedidos e retornando model resumido na listagem

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoResumoModelAssembler.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.api.assembler;
+
+import com.algaworks.algafood.api.model.PedidoResumoModel;
+import com.algaworks.algafood.domain.model.Pedido;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class PedidoResumoModelAssembler {
+
+    private ModelMapper modelMapper;
+    
+    public PedidoResumoModel toModel(Pedido pedido) {
+        return modelMapper.map(pedido, PedidoResumoModel.class);
+    }
+    
+    public List<PedidoResumoModel> toCollectionModel(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
+    }
+    
+}
